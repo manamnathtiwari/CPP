6a67f0e150d5f33eb3c978bf2cc73957de20e3f2
Manamnath Tiwari <hackathonmanam@gmail.com>
2026-06-15 23:59:16 +0530

    Learnt about subarrays

---
+++ c++ STL Ask Senior/Understanding_subarrays.cpp
@@ -0,0 +1,53 @@
+// Subarrays are continous in nature unlike subsequence 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+const i64 INF = 1e18;
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+int main() {
+    auto begin = chrono::high_resolution_clock::now();
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    //Code Starts Here 
+    i64 n;cin>>n;
+    vector<i64> a;
+    for(i64 i=0;i<n;++i){
+    	i64 temp;cin>>temp;
+    	a.push_back(temp);
+	}
+	for(i64 l =0;l<n;++l){
+    	for(i64 r =l;r<n;++r){
+    		// i64 sum =0;
+    		for(i64 k=l;k<=r;++k){
+    			cout<<a[k]<<" ";
+    			// sum += a[r];
+    		}
+    		cout<<"\n";
+        	// cout << "sum = "<<sum << "\n";
+    	}
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    //Questions will be written completely in the last
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
+
+/*
+
+
+
+*/
